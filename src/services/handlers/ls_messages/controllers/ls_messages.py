347c985851f617bf1145f44a23610e01e30e713c
Jiikken <Jiken.rf@yandex.ru>
2025-12-02 16:39:08 +0700

    Разделение ответственности метода handler_ls_messages

--- src/services/handlers/ls_messages/controllers/ls_messages.py
+++ src/services/handlers/ls_messages/controllers/ls_messages.py
@@ -1,16 +1,34 @@
 import logging
 import traceback
 
+from src.services.handlers.ls_messages.commands import CommandsInLS
+from src.services.handlers.ls_messages.models.send_images import SendImagesModel
+from src.services.handlers.posts.ls.controllers.posts import handler_commands_for_posts_in_ls
 from src.services.models.senders import Senders
 from src.services.models.users import Users
-from src.services.handlers.ls_messages.models.send_images import SendImagesModel
 from src.utils.keyboards import Keyboards
-from src.services.handlers.ls_messages.commands import CommandsInLS
-from src.services.handlers.posts.ls.controllers.posts import handler_commands_for_posts_in_ls
 
 
 class HandlerLSMessages(CommandsInLS):
-    def handler_ls_messages(self, msg, user_id, event):
+    def handler_ls_messages(self, msg: str, user_id: int, event: str):
+        forward_message = self._find_command(msg)
+
+        if forward_message:
+            command = self.commands_for_posts_admin.get(forward_message)
+            try:
+                command["handler"](user_id, event)
+            except Exception as e:
+                Senders.sender_in_ls(user_id, f"Произошла ошибка при обращении к методу")
+                logging.error(f"Ошибка при выполнении команды {forward_message}: {e}\n{traceback.format_exc()}")
+
+        elif msg.lower() == "хочу":
+            SendImagesModel.send_cheburek(user_id)
+        elif msg.lower() == "пельмень":
+            SendImagesModel.send_dikiy_ogyrec(user_id)
+        else:
+            Senders.sender_in_ls(user_id, f"Здравствуйте, {Users.info_user(user_id)}\n\nХотите чебурек?", keyboard=Keyboards.cheburek())
+
+    def _find_command(self, msg: str) -> str:
         forward_message = None
 
         for cmd in self.commands_for_posts_admin:
@@ -20,19 +38,6 @@ class HandlerLSMessages(CommandsInLS):
         if forward_message is None:
             handler_commands_for_posts_in_ls.handler_commands_for_posts()
 
-        if forward_message:
-            command = self.commands_for_posts_admin.get(forward_message)
-
-            try:
-                command["handler"](user_id, event)
-            except Exception as e:
-                Senders.sender_in_ls(user_id, f"Произошла ошибка при обращении к методу")
-                logging.error(f"Ошибка при выполнении команды {forward_message}: {e}\n{traceback.format_exc()}")
-        elif msg.lower() == "хочу":
-            SendImagesModel.send_cheburek(user_id)
-        elif msg.lower() == "пельмень":
-            SendImagesModel.send_dikiy_ogyrec(user_id)
-        else:
-            Senders.sender_in_ls(user_id, f"Здравствуйте, {Users.info_user(user_id)}\n\nХотите чебурек?", keyboard=Keyboards.cheburek())
+        return forward_message
 
 handler_ls_messages = HandlerLSMessages()
