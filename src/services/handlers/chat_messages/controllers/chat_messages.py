7fd91b85d527cdfe32ef03777fdb5e5fdd2a81a2
Jiikken <Jiken.rf@yandex.ru>
2025-12-02 16:33:37 +0700

    Разделение ответственности метода handler_chat_message

--- src/services/handlers/chat_messages/controllers/chat_messages.py
+++ src/services/handlers/chat_messages/controllers/chat_messages.py
@@ -1,5 +1,6 @@
 import logging
 import traceback
+import json
 
 from src.database.operations.admins import admins
 from src.services.models.senders import Senders
@@ -10,20 +11,7 @@ from src.services.handlers.chat_messages.commands import CommandsInChat
 handler_commands_for_posts_in_chat = HandlerCommandsForPostsInChat()
 class HandlerChatMessages(CommandsInChat):
     def handler_chat_message(self, msg, user_id, chat_id, event):
-        forward_command = None
-
-        for cmd in self.strict_commands:
-            if msg.lower() == cmd.lower():
-                forward_command = msg
-
-            else:
-                for _cmd in self.not_strict_commands:
-                    if _cmd.lower() in msg.lower():
-                        forward_command = _cmd
-                    
-                    else:
-                        handler_commands_for_posts_in_chat.handler_commands_for_posts(msg, user_id, chat_id, event)
-                        return
+        forward_command = self._find_command(msg, user_id, chat_id, event)
 
         if forward_command:
             command = self.strict_commands.get(forward_command) or self.not_strict_commands.get(forward_command)
@@ -32,28 +20,51 @@ class HandlerChatMessages(CommandsInChat):
                 Senders.sender(chat_id, f"У Вас нет доступа к этой команде")
 
             else:
-                params = {}
-
-                for param in command["params"]:
-                    if param == "chat_id":
-                        params["chat_id"] = chat_id
-                    elif param == "msg":
-                        params["msg"] = msg
-                    elif param == "user_id":
-                        params["user_id"] = user_id
-                    elif param == "event":
-                        params["event"] = event
-                    elif param == "type1":
-                        params["type"] = 1
-                    elif param == "type2":
-                        params["type"] = 2
-                    elif param == "type3":
-                        params["type"] = 3
-
+                params = self._fill_dictionary(command, chat_id, msg, user_id, event)
                 try:
                     command["handler"](**params)
                 except Exception as e:
                     Senders.sender(chat_id, f"Произошла ошибка при обращении к методу")
                     logging.error(f"Ошибка при выполнении команды {forward_command}: {e}\n{traceback.format_exc()}")
+
+    def _find_command(self, msg: str, user_id: int, chat_id: int, event: str) -> str:
+        forward_command = None
+
+        for cmd in self.strict_commands:
+            if msg.lower() == cmd.lower():
+                forward_command = msg
+                break
+
+        if forward_command is None:
+            for _cmd in self.not_strict_commands:
+                if _cmd.lower() in msg.lower():
+                    forward_command = _cmd
+                    break
+
+            handler_commands_for_posts_in_chat.handler_commands_for_posts(msg, user_id, chat_id, event)
+
+        return forward_command
+
+    @staticmethod
+    def _fill_dictionary(command: dict, chat_id: int, msg: str, user_id: int, event: str) -> dict:
+        params = {}
+
+        for param in command["params"]:
+            if param == "chat_id":
+                params["chat_id"] = chat_id
+            elif param == "msg":
+                params["msg"] = msg
+            elif param == "user_id":
+                params["user_id"] = user_id
+            elif param == "event":
+                params["event"] = event
+            elif param == "type1":
+                params["type"] = 1
+            elif param == "type2":
+                params["type"] = 2
+            elif param == "type3":
+                params["type"] = 3
+
+        return params
 
 handler_chat_messages = HandlerChatMessages()
