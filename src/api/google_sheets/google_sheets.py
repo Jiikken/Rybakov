a14771000d0717a0dbc17c4c699b57c7cf6f1c6d
Jiikken <Jiken.rf@yandex.ru>
2025-12-04 14:53:22 +0700

    Добавил признак приватности

--- src/api/google_sheets/google_sheets.py
+++ src/api/google_sheets/google_sheets.py
@@ -16,15 +16,15 @@ class GoogleSheets:
     def __init__(self):
         """Подключение к Google Sheets"""
         try:
-            self.scope = [
+            self._scope = [
                 'https://www.googleapis.com/auth/spreadsheets',
                 'https://www.googleapis.com/auth/drive'
             ]
-            self.creds = Credentials.from_service_account_file(config.credentials_path, scopes=self.scope)
-            self.client = gspread.authorize(self.creds)
+            self._creds = Credentials.from_service_account_file(config.credentials_path, scopes=self._scope)
+            self._client = gspread.authorize(self._creds)
             logging.info(f"Подключение к Google Sheets успешно")
-            self.last_request_time = 0
-            self.min_interval = 1.0
+            self._last_request_time = 0
+            self._min_interval = 1.0
 
             self.manager = SpreadsheetManager()
         except Exception as e:
@@ -33,17 +33,17 @@ class GoogleSheets:
     def rate_limit(self) -> None:
         """Ограничение частоты запросов"""
         current_time = time.time()
-        time_since_last = current_time - self.last_request_time
-        if time_since_last < self.min_interval:
-            time.sleep(self.min_interval - time_since_last)
-        self.last_request_time = time.time()
+        time_since_last = current_time - self._last_request_time
+        if time_since_last < self._min_interval:
+            time.sleep(self._min_interval - time_since_last)
+        self._last_request_time = time.time()
 
     def _get_sheet_with_retry(self, sheet_name=None, max_retries=3) -> Worksheet:
         """Получение листа с повторными попытками"""
         for attempt in range(max_retries):
             try:
                 self.rate_limit()
-                return self.client.open(config.spreadsheetname).worksheet(
+                return self._client.open(config.spreadsheetname).worksheet(
                     sheet_name if sheet_name else config.default_sheet_name)
             except ApiError as e:
                 if '429' in str(e) and attempt < max_retries - 1:
@@ -56,4 +56,4 @@ class GoogleSheets:
 
     def _get_sheet(self, sheet_name: Optional[str] = None) -> Worksheet:
         """Получение таблицы по названию"""
-        return self.client.open(config.spreadsheetname).worksheet(sheet_name if sheet_name else config.default_sheet_name)
+        return self._client.open(config.spreadsheetname).worksheet(sheet_name if sheet_name else config.default_sheet_name)
