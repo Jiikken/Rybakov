08c2fc1eec25934012acb5b3af28fe99cbc7b569
Jiikken <Jiken.rf@yandex.ru>
2025-12-04 18:31:21 +0700

    Добавил комментарии к каждой функции

--- src/services/handlers/chat_messages/models/statisctics.py
+++ src/services/handlers/chat_messages/models/statisctics.py
@@ -3,14 +3,29 @@ from src.api.google_sheets.statistics import statistics_from_gs
 
 class StatisticsModel:
     @staticmethod
-    def redactors_statistics(msg, chat_id):
+    def redactors_statistics(msg: str, chat_id: int):
+        """
+        Информация о статистике редакторов за выбранный период дней (/i)
+
+        :param msg: Сообщение события
+        :param chat_id: ID чата, где произошло событие
+        """
         statistics_from_gs.redactors_statistics(msg, chat_id)
 
     @staticmethod
-    def redactors_statistics_for_admins(chat_id):
+    def redactors_statistics_for_admins(chat_id: int):
+        """
+        Информация о подробной статистике редакторов за определённый период дней (/ai)
+
+        :param chat_id: ID чата, где произошло событие
+        """
         statistics_from_gs.redactors_statistics_for_admins(chat_id)
 
     @staticmethod
-    def reset_stats(chat_id):
-        """Обнуление статистики редакторов"""
+    def reset_stats(chat_id: int):
+        """
+        Обнуление статистики редакторов (/r)
+
+        :param chat_id: ID чата, где произошло событие
+        """
         statistics_from_gs.reset_redactors_statistics(chat_id)
