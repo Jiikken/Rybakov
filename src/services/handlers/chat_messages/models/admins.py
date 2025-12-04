a5415dc629df782842b70621f7d574eb56af3a3b
Jiikken <Jiken.rf@yandex.ru>
2025-12-04 18:23:59 +0700

    Добавил комментарии к каждой функции

--- src/services/handlers/chat_messages/models/admins.py
+++ src/services/handlers/chat_messages/models/admins.py
@@ -5,7 +5,14 @@ from src.database.operations.admins import admins
 
 class AdminsModel:
     @staticmethod
-    def add_admin(chat_id, msg, event):
+    def add_admin(chat_id: int, msg: str, event):
+        """
+        Добавление пользователя в базу данных администраторов (/addmin)
+
+        :param chat_id: ID чата, где произошло событие
+        :param msg: Сообщение события
+        :param event: Событие
+        """
         user_id = users.give_user_id(chat_id, msg, event)
 
         if admins.add_admin_to_db(user_id, chat_id) and user_id is not None:
@@ -15,8 +22,14 @@ class AdminsModel:
             Senders.sender(chat_id, f"{users.info_user(user_id)} уже есть в списке администраторов")
 
     @staticmethod
-    def delete_admin(chat_id, msg, event):
-        """Удаление администратора (/deladm)"""
+    def delete_admin(chat_id: int, msg: str, event):
+        """
+        Удаление пользователя из базы данных администраторов (/deladm)
+
+        :param chat_id: ID чата, где произошло событие
+        :param msg: Сообщение события
+        :param event: Событие
+        """
         user_id = users.give_user_id(chat_id, msg, event)
 
         if admins.remove_admin_from_db(user_id, chat_id) and user_id is not None:
@@ -26,7 +39,12 @@ class AdminsModel:
             Senders.sender(chat_id, f"{users.info_user(user_id)} не найден(а) в списке администратора")
 
     @staticmethod
-    def admins_list(chat_id):
+    def admins_list(chat_id: int):
+        """
+        Отправляет список администраторов из базы данных
+
+        :param chat_id: ID чата, где произошло событие
+        """
         start_label = f"Список администрации:\n\n"
         for cell in admins.get_admins_list():
             if cell is None:
